0d57fc2b9c64b013985171a2575ec94f825fef9a
Akshay <umbarkar.a13@gmail.com>
2022-09-15 10:56:54 +0530

    Developer1 commit

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+com.multiplication;
+public class Multiplication{
+	public static void main(String []args){
+		int a = 10;
+		int b = 30;
+		int c = a * b;
+		System.out.println(" Multiplication is >> "+c);
+	}
+}
